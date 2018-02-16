27a1c5d218780c0f9ceefb9b42abb6c18a189816
Benjamin Earl <benearl72@gmail.com>
2018-02-16 10:02:38 -0700

    put menu back

---
+++ web/exerner/menu.php
@@ -0,0 +1,24 @@
+<?php
+require '_shared.php';
+?>
+
+<div class="ui right fixed vertical menu">
+    <a class="header item" href="index.html">
+        <img class="ui mini image" src="assets/logo.jpg">
+    </a>
+    <a class="item" href="sign-in.html">Sign-in</a>
+    <div class="item">
+        <div class="header">All Posts</div>
+        <div class="menu">
+<?php
+$query = $db->query('SELECT title, id FROM post');
+foreach ($query as $row)
+{
+	echo '<a class="item" href="posts.php?id=' . $row['id'] . '">';
+	echo $row['title'];
+	echo '</a>';
+}
+?>
+        </div>
+    </div>
+</div>
