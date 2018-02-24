e4f425e654b7ab64483b2f8a0b104b917d020269
Benjamin Earl <benearl72@gmail.com>
2018-02-24 13:30:58 -0700

    fixed the get username

--- web/exerner/_getusername.php
+++ web/exerner/_getusername.php
@@ -2,5 +2,6 @@
     require_once '_shared.php';
     $get_user = $db->prepare('SELECT username FROM person WHERE id=:id;');
     $get_user->execute(array('id' => safe_get('id',-1)));
-    echo $get_user->fetch();
+    $row = $get_user->fetch();
+    echo $row['id']
 ?>
